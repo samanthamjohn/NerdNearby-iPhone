1b4cd53350271a87d711978ad6255f7774c0717a
Sam John & Sean Moon <pair+samantha+sean@nerdnearby.com>
2011-06-25 22:29:14 +0000

    initial commit

---
+++ NerdNearby/NerdNearbyAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  NerdNearbyAppDelegate.h
+//  NerdNearby
+//
+//  Created by Samantha John on 6/25/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NerdNearbyAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
